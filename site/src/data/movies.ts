d7aab0ea44f2d224c2ff440c02cb09e739447b88
Cao Lê Gia Phú <23127535@student.hcmus.edu.vn>
2026-04-08 11:20:57 +0700

    Add movie ggdrive

--- site/src/data/movies.ts
+++ site/src/data/movies.ts
@@ -64,7 +64,7 @@ export const movies: Movie[] = [
       "Đại Tiệc Trăng Máu 8 theo chân một vị đạo diễn hay bị coi thường (Vân Sơn đóng) trong dự án thử thách nhất đời ông: thực hiện một bộ phim dài 35 phút chỉ với một cú máy. Hàng loạt tình huống dở khóc dở cười xảy ra khi các diễn viên liên tục gây chuyện “khó đỡ”. Thế nhưng, việc hoàn thành tác phẩm là cơ hội cuối cùng để ông giành lại sự tôn trọng từ cô con gái đam mê nghệ thuật. Quy tụ dàn sao đình đám nhất điện ảnh Việt, Đại Tiệc Trăng Máu 8 ấn định lịch khai tiệc vào 24.04.2026, chiếu xuyên Đại lễ Giỗ Tổ Hùng Vương & 30.04.2026.",
     posterUrl: "/poster/Dai-tiec-trang-mau-8.jpg",
     videoUrl:
-      "https://drive.google.com/file/d/1vLd_HlZrZ06EENY_tdcgZQF6c8n8FXj0/view?usp=sharing",
+      "https://drive.google.com/file/d/10fiPcarXLu1sB3qRqv0oaX2gSKpNg65d/view?usp=sharing",
     durationMinutes: 114,
   },
   {
